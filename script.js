67c2c92001d18375d3de1b859063ba49733cee17
Will <will62smith@gmail.com>
2023-01-13 18:43:18 +0000

    Added number caching functionality
    User number input is now stored in a string through concatenation

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 /*eslint-env es6*/
 
-function myFunction() {
+let number_entered = "";
+
+function addNumber(number) {
+    number_entered += number.toString();
     const name = document.getElementById("test")
     name.style.color = "red";
+    console.log(number_entered);
+}
+
+function clear_number() {
+    number_entered = "";
+    console.log(number_entered);
+}
+
+function remove() {
+    if (number_entered.length > 0) {
+        number_entered = number_entered.substring(0, number_entered.length-1);
+    }
+    console.log(number_entered);
+}
+
+function add(){
+    
+}
+
+function minus(){
+    
+}
+
+function multiply(){
+    
+}
+
+function divide(){
+    
 }
